88d7da868cb2cf7f4e8203190bd824530e33e49f
Fikri <muhamadfikri64@yahoo.co.id>
2021-04-04 10:42:57 +0700

    adding error page

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,23 @@
-import Link from "next/link";
 import Layout from "../components/Layout";
+import Error from "./_error";
 import fetch from "isomorphic-unfetch";
 import {Component} from "react";
 
 export default class About extends Component {
     static async getInitialProps() {
         const res = await fetch("https://api.github.com/users/fikrifire19");
+        const statusCode = res.status > 200 ? res.status : false;
         const data = await res.json();
 
-        return {user:data};
+        return {user:data, statusCode};
     }
 
     render() {
-        const {user} = this.props;
+        const {user, statusCode} = this.props;
+
+        if (statusCode) {
+            return <Error statusCode={statusCode} />
+        }
         
         return (
             <Layout title="About">
